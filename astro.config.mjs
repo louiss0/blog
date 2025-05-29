9f06db9abbf6504338e50aac87bf48737d92e76b
louiss0 <louisshelton0@gmail.com>
2025-05-29 19:28:25 -0400

    add: necessary integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import markdoc from '@astrojs/markdoc';
+import vue from '@astrojs/vue';
+import unocss from '@unocss/astro';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap(), markdoc(), vue(), unocss()],
 });
