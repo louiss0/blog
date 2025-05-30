3d227317e25f9634424e4c338d50d2ed22f3dbe2
louiss0 <louisshelton0@gmail.com>
2025-05-29 21:06:48 -0400

    refactor: add more necessary dependencies add more missing files setup content collections

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
-import markdoc from '@astrojs/markdoc';
-import vue from '@astrojs/vue';
-import unocss from '@unocss/astro';
+import { defineConfig } from "astro/config";
+import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
+import markdoc from "@astrojs/markdoc";
+import vue from "@astrojs/vue";
+import unocss from "@unocss/astro";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(), markdoc(), vue(), unocss()],
+  integrations: [
+    icon({
+      include: { mdi: ["*"], fa: ["*"] },
+    }),
+    sitemap(),
+    markdoc(),
+    vue({ jsx: true }),
+    unocss(),
+  ],
 });
--- src/collections.ts
+++ src/collections.ts
@@ -1,124 +1,140 @@
 import { baseSchema } from "./schemas";
 import { z, defineCollection, type SchemaContext } from "astro:content";
-import { Enum, type InferValue } from "better-enums";
 
-export const CATEGORIES = Enum(["Segment", "Tutorial", "Standalone"])
+export const CATEGORIES = z.enum(["Segment", "Tutorial", "Standalone"]);
 
-export type Category = InferValue<typeof CATEGORIES>
-
+export type Category = z.infer<typeof CATEGORIES>;
 
 const categoryWithPartAndSeriesSchema = z.object({
-    category: z.literal(CATEGORIES.accessor.Segment),
-    series: z.string({
-        required_error: "A post that is a segment must have a series associated with it",
+  category: z.literal(CATEGORIES.Values.Segment),
+  series: z
+    .string({
+      required_error:
+        "A post that is a segment must have a series associated with it",
     })
-        .max(45)
-        .regex(
-            /\b[A-Z][A-Za-z0-9\s]*\b/,
-            `The series must start with an uppercase and end with a word.
-            It can only have characters and spaces in it.   
-            `
-        )
-        .transform((string) => `${string}:`),
-    part: z.number({
-        required_error: "A post with a segment must have a part number associated with it"
-    }).int()
+    .max(45)
+    .regex(
+      /\b[A-Z][A-Za-z0-9\s]*\b/,
+      `The series must start with an uppercase and end with a word.
+            It can only have characters and spaces in it.
+            `,
+    )
+    .transform((string) => `${string}:`),
+  part: z
+    .number({
+      required_error:
+        "A post with a segment must have a part number associated with it",
+    })
+    .int(),
 });
 
 const categoryWithoutPartAndSeriesSchema = z.object({
-    category: z.literal(CATEGORIES.accessor.Standalone)
-        .or(z.literal(CATEGORIES.accessor.Tutorial)),
-    part: z.undefined(),
-    series: z.undefined()
+  category: z
+    .literal(CATEGORIES.Values.Standalone)
+    .or(z.literal(CATEGORIES.Values.Tutorial)),
+  part: z.undefined(),
+  series: z.undefined(),
 });
 
-
-/* 
+/*
  TODO: Remember to add images to based on each topic
  TODO: Use imported images as default's for all schema.
 
 */
 
-const seriesSchema = categoryWithPartAndSeriesSchema
-    .or(categoryWithoutPartAndSeriesSchema)
+const seriesSchema = categoryWithPartAndSeriesSchema.or(
+  categoryWithoutPartAndSeriesSchema,
+);
 
+export const postSchema = baseSchema.and(seriesSchema).and(
+  z.object({
+    tags: z.array(z.string()),
+  }),
+);
 
-export const postSchema = baseSchema
-    .and(seriesSchema)
-    .and(z.object({
-        tags: z.array(z.string())
-    }))
-
-
-export type Post = z.infer<typeof postSchema>
-
+export type Post = z.infer<typeof postSchema>;
 
 const getCreatePostSchemaBasedOnIfATagStartsWithPassedInValueAndImageFromSchemaContext =
-    (string: string,) => ({ image }: SchemaContext) =>
-        postSchema
-            .and(z.object({
-                heroImage: image(),
-                tags: z.array(z.string())
-                    .refine(getArrayStartsWithString(string)),
-            }))
-            .transform((postFrontMatter) => {
 
-                if (postFrontMatter.category !== "Segment") {
+    (string: string) =>
+    ({ image }: SchemaContext) =>
+      postSchema
+        .and(
+          z.object({
+            heroImage: image(),
+            tags: z.array(z.string()).refine(getArrayStartsWithString(string)),
+          }),
+        )
+        .transform((postFrontMatter) => {
+          if (postFrontMatter.category !== "Segment") {
+            return postFrontMatter;
+          }
 
-                    return postFrontMatter
-                }
+          const { title, part, series, ...rest } = postFrontMatter;
 
-                const { title, part, series, ...rest } = postFrontMatter
-
-                return {
-                    ...rest,
-                    title: `${series}${title} part ${part}`
-                }
-
-            })
-
-
+          return {
+            ...rest,
+            title: `${series}${title} part ${part}`,
+          };
+        });
 
 export const collections = {
-    astro: defineCollection({
-        schema:
-            getCreatePostSchemaBasedOnIfATagStartsWithPassedInValueAndImageFromSchemaContext("astro")
-    }),
-    html: defineCollection({
-        schema:
-            getCreatePostSchemaBasedOnIfATagStartsWithPassedInValueAndImageFromSchemaContext("HTML")
-    }),
-    typescript: defineCollection({
-        schema:
-            getCreatePostSchemaBasedOnIfATagStartsWithPassedInValueAndImageFromSchemaContext("typescript")
-    }),
-    css: defineCollection({
-        schema:
-            getCreatePostSchemaBasedOnIfATagStartsWithPassedInValueAndImageFromSchemaContext("CSS")
-    }),
-    react: defineCollection({
-        schema:
-            getCreatePostSchemaBasedOnIfATagStartsWithPassedInValueAndImageFromSchemaContext("react")
-    }),
-    vue: defineCollection({
-        schema:
-            getCreatePostSchemaBasedOnIfATagStartsWithPassedInValueAndImageFromSchemaContext("vue")
-    }),
-    "food-for-thought": defineCollection({
-        schema:
-            getCreatePostSchemaBasedOnIfATagStartsWithPassedInValueAndImageFromSchemaContext("food-for-thought")
-    }),
-    "web-development": defineCollection({
-        schema:
-            getCreatePostSchemaBasedOnIfATagStartsWithPassedInValueAndImageFromSchemaContext("web-development")
-    }),
+  astro: defineCollection({
+    schema:
+      getCreatePostSchemaBasedOnIfATagStartsWithPassedInValueAndImageFromSchemaContext(
+        "astro",
+      ),
+  }),
+  html: defineCollection({
+    schema:
+      getCreatePostSchemaBasedOnIfATagStartsWithPassedInValueAndImageFromSchemaContext(
+        "HTML",
+      ),
+  }),
+  typescript: defineCollection({
+    schema:
+      getCreatePostSchemaBasedOnIfATagStartsWithPassedInValueAndImageFromSchemaContext(
+        "typescript",
+      ),
+  }),
+  css: defineCollection({
+    schema:
+      getCreatePostSchemaBasedOnIfATagStartsWithPassedInValueAndImageFromSchemaContext(
+        "CSS",
+      ),
+  }),
+  react: defineCollection({
+    schema:
+      getCreatePostSchemaBasedOnIfATagStartsWithPassedInValueAndImageFromSchemaContext(
+        "react",
+      ),
+  }),
+  vue: defineCollection({
+    schema:
+      getCreatePostSchemaBasedOnIfATagStartsWithPassedInValueAndImageFromSchemaContext(
+        "vue",
+      ),
+  }),
+  "food-for-thought": defineCollection({
+    schema:
+      getCreatePostSchemaBasedOnIfATagStartsWithPassedInValueAndImageFromSchemaContext(
+        "food-for-thought",
+      ),
+  }),
+  "web-development": defineCollection({
+    schema:
+      getCreatePostSchemaBasedOnIfATagStartsWithPassedInValueAndImageFromSchemaContext(
+        "web-development",
+      ),
+  }),
 };
 
-export const COLLECTION_NAMES = Object.keys(collections) as ReadonlyArray<keyof typeof collections>
+export const COLLECTION_NAMES = Object.keys(collections) as ReadonlyArray<
+  keyof typeof collections
+>;
 export type AllCollectionNames = typeof COLLECTION_NAMES;
-export type AllCollectionNamesValues = typeof COLLECTION_NAMES[number];
+export type AllCollectionNamesValues = (typeof COLLECTION_NAMES)[number];
 
 function getArrayStartsWithString(string: string): (arg: string[]) => unknown {
-    return tags => tags.at(0) === string;
-
+  return (tags) => tags.at(0) === string;
 }
