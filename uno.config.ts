6caa2fc6e6a21c263cd0e3ac1f6bfc2258f99c09
louiss0 <louisshelton0@gmail.com>
2025-05-29 21:09:00 -0400

    replace depracated preset for windi

--- uno.config.ts
+++ uno.config.ts
@@ -1,21 +1,16 @@
-import { presetWind, presetTypography, defineConfig } from 'unocss';
-import transformerVariantGroup from '@unocss/transformer-variant-group'
+import { presetWind3, presetTypography, defineConfig } from "unocss";
+import transformerVariantGroup from "@unocss/transformer-variant-group";
 export default defineConfig({
-    presets: [
-        presetWind(),
-        presetTypography()
-    ],
-    transformers: [
-        transformerVariantGroup()
-    ],
-    blocklist: [
-        /^(?:(?:m|p|b)a-)/,
-        /^(?:(?:op|of|position|case|decoration|line-height|b|c)-)/,
-        //^(?:bg|text|border|outline)-(?:[a-z]+[A-Z][a-z]+)/,
-        // /^(?:bg|text|border|outline)-(?:[a-z]+)-[1-9]$/,
-        /^(?:object)-[a-z]{2}$/,
-        /^(grid)-(?!cols|rows)/,
-        /^(flex)-(justify|items|content)/,
-        /^(font|rounded)-\d+/
-    ],
-})
+  presets: [presetWind3(), presetTypography()],
+  transformers: [transformerVariantGroup()],
+  blocklist: [
+    /^(?:(?:m|p|b)a-)/,
+    /^(?:(?:op|of|position|case|decoration|line-height|b|c)-)/,
+    //^(?:bg|text|border|outline)-(?:[a-z]+[A-Z][a-z]+)/,
+    // /^(?:bg|text|border|outline)-(?:[a-z]+)-[1-9]$/,
+    /^(?:object)-[a-z]{2}$/,
+    /^(grid)-(?!cols|rows)/,
+    /^(flex)-(justify|items|content)/,
+    /^(font|rounded)-\d+/,
+  ],
+});
